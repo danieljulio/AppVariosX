e335237abced954ab97bffd27f7b1237c0b3d4d0
Daniel Julio <informaticadanieljulio@gmail.com>
2017-09-06 22:25:29 -0300

    06/09/17/quarta

---
+++ AppVarios/AppVarios/AppVarios/Telas/CadastrarLivro.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppVarios.Telas;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppVarios.Telas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CadastrarLivro : ContentPage
+	{
+		public CadastrarLivro ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ AppVarios/AppVarios/AppVarios/Telas/TelaInicio.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppVarios.Telas;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppVarios.Telas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TelaInicio : ContentPage
+	{
+        public async void botaoLivro_Clicked(object sender, EventArgs e)
+        {
+            Telas.CadastrarLivro.Show();
+        }
+		public TelaInicio ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
